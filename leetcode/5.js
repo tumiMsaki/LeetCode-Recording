4a55e0060402b5d130ae4ecd1d289d13fb3636c2
tumiMsaki <245551699@qq.com>
2019-11-06 00:46:11 +0800

    init

--- leetcode/5.js
+++ leetcode/5.js
@@ -3,5 +3,29 @@
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    
+  if (!s || s.length === 0) return s
+  if (s.length === 1) return s[0]
+  let result = ''
+
+  var calPalindromeIndex = function(left,right,s){
+    let len = s.length;
+    while(left>=0&&right<len&&s[left] == s[right]){
+      left--;
+      right++;
+    }
+    return {left:left+1,right:right}
+  }
+  for(let i = 0,len = s.length;i<len;i++){
+    let even = '';
+    let odd = '';
+    if(s[i] == s[i+1]){
+      let evenIndex = calPalindromeIndex(i-1,i+2,s);
+      even = s.slice(evenIndex.left,evenIndex.right);
+    }
+    let oddIndex = calPalindromeIndex(i-1,i+1,s);
+    odd = s.slice(oddIndex.left,oddIndex.right);
+    let re = odd.length>even.length?odd:even;
+    result = result.length>re.length?result:re;
+  }
+  return result;
 };
