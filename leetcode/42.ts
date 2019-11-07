0f5785895d121395c059d9ababe161044f98c0d4
tumiMsaki <245551699@qq.com>
2019-11-07 15:47:54 +0800

    init

--- leetcode/42.ts
+++ leetcode/42.ts
@@ -1,7 +1,25 @@
-/**
+/** 接雨水
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
-    
-};
+var trap = function(height: number[]): number {
+  let left = 0
+  let right = height.length - 1
+  let leftMax = 0
+  let rightMax = 0
+  let count = 0
+  while (left <= right) {
+    leftMax = Math.max(leftMax, height[left])
+    rightMax = Math.max(rightMax, height[right])
+    if (leftMax < rightMax) {
+      count += leftMax - height[left]
+      left ++ 
+    } else {
+      count += rightMax - height[right]
+      right --
+    }
+  }
+  return count
+}
+
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
