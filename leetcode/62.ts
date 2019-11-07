259ef290a882691f56460194d750694820eb1881
tumiMsaki <245551699@qq.com>
2019-11-07 19:31:57 +0800

    init

--- leetcode/62.ts
+++ leetcode/62.ts
@@ -4,15 +4,25 @@
  * @return {number}
  */
 var uniquePaths = function(m: number, n: number): number{
-  let arr: Array<number> = []
-  arr.length = 10
-  arr.fill(1)
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      arr[j] += arr[j-1]
+  let temp = []
+  for (let i = 0; i < n; i++) {
+    temp[i] = Array(m).fill(0)
+  }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (i == 0 && j == 0) {
+        temp[i][j] = 1;
+      } else if (i == 0) {
+        temp[i][j] = 1;
+      } else if (j == 0) {
+        temp[i][j] = 1;
+      } else {
+        temp[i][j] = temp[i - 1][j] + temp[i][j - 1]
+      }
     }
   }
-  return arr[-1]
+
+  return temp[n - 1][m - 1]
 }; 
 
 console.log(uniquePaths(3, 4))
