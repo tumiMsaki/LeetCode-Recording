8e7c67bc80b12a8b2abe9c1606536c9c5e70787c
tumiMsaki <245551699@qq.com>
2019-11-20 19:34:22 +0800

    fix

--- leetcode/1.ts
+++ leetcode/1.ts
@@ -1,18 +1,19 @@
 interface Iresult {
-  [propName: number] :number
+  [propName: number]: number
 }
 /** 两数相加
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums: number[], target: number):number[] {
-    let result: Iresult
-    for (let i = 0, len = nums.length; i < len; i++) {
-      let diff = target - nums[i]
-      if (result[diff] !== undefined) {
-        return [result[diff], i]
-      }
-      result[nums[i]] = i
+var twoSum = function(nums: number[], target: number): number[] {
+  let arr: number[] = []
+  for (let i = 0, len = nums.length; i < len; i++) {
+    let diff = target - nums[i]
+    if (arr[diff] !== undefined) {
+      return [arr[diff], i]
     }
-};
+    arr[nums[i]] = i
+  }
+  return
+}
