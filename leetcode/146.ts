9fc80fffb2f385cb58c0992b6a23dc320190fa78
tumiMsaki <245551699@qq.com>
2019-11-26 21:04:24 +0800

    fix

---
+++ leetcode/146.ts
@@ -0,0 +1,30 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity: number) {
+
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key: number):number {
+    return
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key: number, value: number):void {
+    
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
