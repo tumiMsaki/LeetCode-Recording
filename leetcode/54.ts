cd3bdd8a4f177ee0ac45af1bce3ad6d4c6e348c8
tumiMsaki <245551699@qq.com>
2019-11-26 20:09:24 +0800

    fix

--- leetcode/54.ts
+++ leetcode/54.ts
@@ -3,35 +3,39 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix: number[][]): number[] {
-  if (matrix.length === 0 || matrix[0].length === 0) return []
-  let arr = []
-  let m = matrix[0].length // 1
-  let n = matrix.length // 2
-  let count = m * n
-  let index = 0
-  let max = Math.min(m, n)
-  while (index <= max) {
-    for (let i = index; i < m - index; i++) {
-      arr.push(matrix[index][i])
+  let result: number[] = []
+  if (matrix.length === 0) return result
+
+  let c1 = 0,
+      c2 = matrix[0].length - 1,
+      r1 = 0,
+      r2 = matrix.length - 1
+  
+  while (c1 <= c2 && r1 <= r2) {
+    for (let c = c1; c <= c2; c++) {
+      console.log(1)
+      result.push(matrix[r1][c])
     }
-    for (let i = index + 1; i < n - index - 1; i++) {
-      arr.push(matrix[i][m - index - 1])
+    for (let r = r1 + 1; r <= r2; r++) {
+      console.log(2)
+      result.push(matrix[r][c2])
     }
-    if (arr.length > count - 1 ) {
-      return arr
+    if (c1 < c2 && r1 < r2) {
+      for (let c = c2 - 1; c > c1; c--) {
+        console.log(3)
+        result.push(matrix[r2][c])
+      }
+      for (let r = r2; r > r1; r--) {
+        console.log(4)
+        result.push(matrix[r][c1])
+      }
     }
-    for (let i = m - index - 1; i >= index; i--) {
-      arr.push(matrix[n - index - 1][i])
-    }
-    if (arr.length > count - 1 ) {
-      return arr
-    }
-    for (let i = n - index - 2; i > index; i--) {
-      arr.push(matrix[i][index])
-    }
-    index ++
+    r1++
+    r2--
+    c1++
+    c2--
   }
-  return arr
+  return result
 }
 
-console.log(spiralOrder([[7],[9],[6]]))
+console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
