759e6f9629b2d010a74ca85c9f452fad122abbda
tumiMsaki <245551699@qq.com>
2019-11-29 17:42:57 +0800

    fix

---
+++ leetcode/23.ts
@@ -0,0 +1,43 @@
+class ListNode {
+  val: number
+  next: ListNode
+  constructor(val: number) {
+    this.val = null
+    this.next = null
+  }
+}
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists: ListNode[]): ListNode {
+  let len = lists.length
+  let newList = new ListNode(0)
+  let current = newList
+  let index = 0
+  let num = lists[0].val
+  while (lists.length > 0) {
+    for (let i = 0; i < len; i++) {
+      if (num >= lists[i].val) {
+        num = lists[i].val
+        index = i
+      }
+    }
+    current.next = new ListNode(lists[index].val)
+    current = current.next
+    if (lists[index].next) {
+      lists[index] = lists[index].next
+    } else {
+      lists.splice(index, 1)
+    }
+  }
+  return newList.next
+}
