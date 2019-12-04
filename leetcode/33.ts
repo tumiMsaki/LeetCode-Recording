0b73a29c4148c5e52cd6023d496a6ab0f963dd5e
tumiMsaki <245551699@qq.com>
2019-12-04 19:39:49 +0800

    fix

--- leetcode/33.ts
+++ leetcode/33.ts
@@ -7,30 +7,30 @@ var search = function(nums: number[], target: number): number {
   if (nums.length < 1) return -1
   let left = 0
   let right = nums.length - 1 //6
-  while (left < right) {
-    let mid = Math.floor((left + right) / 2)
+  while (left <= right) {
+    let mid = Math.floor((right + left) / 2)
+    if (nums[mid] === target) {
+      return mid
+    }
     console.log(mid)
     if (nums[mid] < nums[right]) {
-      if (target > nums[mid] && target < nums[right]) {
-        right = mid - 1
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1
       } else {
-        left = mid
+        right = mid - 1
       }
     } else {
       if (target >= nums[left] && target < nums[mid]) {
         right = mid - 1
       } else {
-        left = mid
+        left = mid + 1
       }
     } 
   }
-  if (nums[left] !== target) {
-    return -1
-  }
-  return left
+  return -1
 }
 
-console.log(search([2,3,4,5,6,7,0],0))
+console.log(search([1,3],1))
 
 //[4,5,6,7,0,1,2]
 //[5,6,7,0,1,2,3]
